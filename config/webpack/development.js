81f84aef1faf5f7192160fbe31d9927d047c6e2c
vlpro <vlpro@me.com>
2020-03-01 15:22:03 +0500

    Pass API_URL environment variable into the code

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -27,6 +27,7 @@ module.exports = {
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+			'process.env.API_URL': JSON.stringify(process.env.API_URL),
 		}),
 	],
 };
