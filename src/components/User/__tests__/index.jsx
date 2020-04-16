af97f3bd6cc182fcbdda25a0a5839090806a3583
Pons <suyambu.vps@gmail.com>
2020-04-17 00:53:10 +0200

    Add `should render Breadcrumbs after NavBar` for User component

--- src/components/User/__tests__/index.jsx
+++ src/components/User/__tests__/index.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NavBar from 'components/NavBar';
+import { useUser } from 'store/store';
 import User from '../index';
+import Breadcrumbs from 'components/Breadcrumbs';
+
+jest.mock('store/store');
+
 
 describe('User', () => {
+  const mockUser = {
+    display_name: 'Steichen, Joelle',
+    email: 'Joelle.Steichen1@T-Mobile.com',
+  };
+
+  const logUser = (user) => {
+    console.log(user.children().debug());
+  }
+
+  const expectBreadcrumbsAsSecondChild = (user) => {
+    expect(user.childAt(1).equals(<Breadcrumbs paths={[{ text: 'Home', path: '/' }]}/>)).toBeTruthy();
+  };
+
   const expectNavBarAsFirstChild = (user) => {
-    console.log(user.children().debug())
     expect(user.childAt(0).equals(<NavBar />)).toBeTruthy();
   };
 
   it('should render nav bar as first element', () => {
     const user = shallow(<User />);
+    expectNavBarAsFirstChild(user);
+  });
+
+  it('should render Breadcrumbs after NavBar', () => {
+    // useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
 
     expectNavBarAsFirstChild(user);
+    logUser(user)
+    expectBreadcrumbsAsSecondChild(user);
   });
 });
