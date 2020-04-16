ec8d8285e2db2495dbc021973166e799829cf1fc
Deepa Sajani Jeyaraj <deepasajani.j@gmail.com>
2020-04-16 23:30:30 +0200

    Add jest module mapper for aliases and to mock images

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
+    '\\.(png|jpg)$': '<rootDir>/__mocks__/mockFile.js',
+    '^components(.*)$': '<rootDir>/src/components$1',
+    '^config$': '<rootDir>/src/config',
+    '^store(.*)$': '<rootDir>/src/store$1',
+    '^assets(.*)$': '<rootDir>/src/assets$1',
   },
+  // collectCoverageFrom: [
+  //   '**/src/**/*.{js,jsx}',
+  // ],
 };
--- src/components/Breadcrumbs/__tests__/index.jsx
+++ src/components/Breadcrumbs/__tests__/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Breadcrumbs from '../index';
+import Breadcrumbs from 'components/Breadcrumbs';
 
 describe('Breadcrumbs', () => {
   const expectRootElementToBeDivWithClassName = (breadcrumb) => {
