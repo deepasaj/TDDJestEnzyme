e2470fbd6fb45891d3ca46ecbc6963de1c68fe92
Pons <suyambu.vps@gmail.com>
2020-04-17 12:03:05 +0200

    Add all test cases for User component

--- src/components/User/__tests__/index.jsx
+++ src/components/User/__tests__/index.jsx
@@ -7,11 +7,19 @@ import Breadcrumbs from 'components/Breadcrumbs';
 
 jest.mock('store/store');
 
+//TODO: what is <style> at line no 27
 
 describe('User', () => {
+
+
+  //<editor-fold desc="Custom expects">
   const mockUser = {
     display_name: 'Steichen, Joelle',
     email: 'Joelle.Steichen1@T-Mobile.com',
+    avatar: 'any url',
+    first_name: ' a first name',
+    last_name: 'a last name',
+    username: 'a user name'
   };
 
   const logUser = (user) => {
@@ -25,6 +33,121 @@ describe('User', () => {
   const expectNavBarAsFirstChild = (user) => {
     expect(user.childAt(0).equals(<NavBar />)).toBeTruthy();
   };
+
+  const expectMainBlock = (user) => {
+    let main = user.childAt(2);
+    expect(main.type()).toEqual('main');
+    expect(main.prop('role')).toEqual('main');
+    expect(main.prop('id')).toEqual('main');
+    expect(main.prop('className')).toEqual('container');
+  };
+
+  const expectJustifyContentDiv = (user) => {
+    let main = user.childAt(2);
+    let justfyContentDiv = main.childAt(1);
+    expect(justfyContentDiv.type()).toEqual('div');
+    expect(justfyContentDiv.prop('className')).toEqual('row justify-content-md-center');
+  };
+
+  const expectCardDiv = (user) => {
+    let main = user.childAt(2);
+    let cardDiv = main.childAt(1).children();
+    expect(cardDiv.type()).toEqual('div');
+    expect(cardDiv.prop('className')).toEqual('card');
+  };
+
+  const expectCardDivNotRendered = (user) => {
+    let main = user.childAt(2);
+    let justfyContentDiv = main.childAt(1);
+    console.log(justfyContentDiv.debug())
+    expect(justfyContentDiv.children().length).toBe(0);
+  };
+
+  const expectAvatar = (user) => {
+    let main = user.childAt(2);
+    let cardDiv = main.childAt(1).children();
+    let cardHeader = cardDiv.childAt(0);
+    let avatarDiv = cardHeader.childAt(0);
+    const mockAvatarUrl = () => `url("${mockUser.avatar}")`;
+
+    expect(cardHeader.type()).toEqual('div');
+    expect(cardHeader.prop('className')).toEqual('card-header text-center');
+    expect(avatarDiv.type()).toEqual('div');
+    expect(avatarDiv.prop('className')).toEqual('user-img-shape');
+    console.log(avatarDiv.prop('style'))
+    expect(avatarDiv.prop('style')).toEqual({ backgroundImage: mockAvatarUrl() });
+  };
+
+  const expectUserName = (user) => {
+    let main = user.childAt(2);
+    let cardDiv = main.childAt(1).children();
+    let cardBody = cardDiv.childAt(1);
+
+    expect(cardBody.type()).toEqual('div');
+    expect(cardBody.prop('className')).toEqual('card-body');
+    expect(cardBody.childAt(0).equals(
+      <ul>
+        <strong>Username: </strong>
+        {mockUser.username}
+      </ul>
+      )
+    ).toBeTruthy();
+  };
+
+  const expectFirstName = (user) => {
+    let cardBody = user.childAt(2).childAt(1).children().childAt(1);
+
+    expect(cardBody.childAt(1).equals(
+      <ul>
+        <strong>First name: </strong>
+        {mockUser.first_name}
+      </ul>
+      )
+    ).toBeTruthy();
+  };
+
+  const expectLastName = (user) => {
+    let cardBody = user.childAt(2).childAt(1).children().childAt(1);
+
+    expect(cardBody.childAt(2).equals(
+      <ul>
+        <strong>Last name: </strong>
+        {mockUser.last_name}
+      </ul>
+      )
+    ).toBeTruthy();
+  };
+
+  const expectDisplayName = (user) => {
+    let cardBody = user.childAt(2).childAt(1).children().childAt(1);
+
+    expect(cardBody.childAt(3).equals(
+      <ul>
+        <strong>Display name: </strong>
+        {mockUser.display_name}
+      </ul>
+      )
+    ).toBeTruthy();
+  };
+
+
+  const expectEmail = (user) => {
+    let cardBody = user.childAt(2).childAt(1).children().childAt(1);
+
+    expect(cardBody.childAt(4).equals(
+      <ul>
+        <strong>Email: </strong>
+        {mockUser.email}
+      </ul>
+      )
+    ).toBeTruthy();
+  };
+
+  //</editor-fold>
+
+  beforeEach(() => {
+    useUser.mockReturnValue(null);
+  });
 
   it('should render nav bar as first element', () => {
     const user = shallow(<User />);
@@ -47,6 +170,81 @@ describe('User', () => {
       { text: 'Home', path: '/' },
       { text: `${mockUser.display_name}`, path: '/me' },
     ]);
+  });
+
+  it("should renter main block", () => {
+    const user = shallow(<User />);
+
+    expectMainBlock(user)
+  });
+
+  it("should renter a div inside main to justify content", () => {
+    const user = shallow(<User />);
+
+    expectJustifyContentDiv(user)
+  });
+
+  it("should render card div if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectCardDiv(user)
+  });
+
+  it("should not render card div if user has not logged in", () => {
+    const user = shallow(<User />);
+
+    expectCardDivNotRendered(user)
+  });
+
+  it("should render avatar if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectAvatar(user)
+  });
+
+  it("should render user name if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectUserName(user)
+  });
+
+  it("should render first name if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectFirstName(user)
+  });
+
+
+  it("should render last name if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectLastName(user)
+  });
+
+  it("should render display name if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectDisplayName(user)
+  });
+
+  it("should render email if user has logged in", () => {
+    useUser.mockReturnValue(mockUser);
+
+    const user = shallow(<User />);
+
+    expectEmail(user)
   });
 
 });
